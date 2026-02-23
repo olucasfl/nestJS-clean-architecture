d3c4efb538612c4ae8230402ae54e9c3c505bbc3
Lucas Farias Leandro <lucas.leandro@unimedjp.corp>
2026-02-23 12:45:33 -0300

    feat: implement abstract method for User ID

---
+++ src/shared/domain/entities/entity.ts
@@ -0,0 +1,22 @@
+import { randomUUID as uuid } from 'node:crypto';
+
+export abstract class Entity<Props = any> {
+  public readonly _id: string;
+  public readonly props: Props;
+
+  constructor(props: Props, id?: string) {
+    this._id = id ?? uuid();
+    this.props = props;
+  }
+
+  get id() {
+    return this._id;
+  }
+
+  toJSON(): Required<{ id: string } & Props> {
+    return {
+      id: this._id,
+      ...this.props,
+    } as Required<{ id: string } & Props>;
+  }
+}
